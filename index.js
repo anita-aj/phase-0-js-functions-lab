1d028f26896f6d8c15f019a0fe78a2a9d6fae4c4
Anita Jepngetich <anita.jepngetich@student.moringaschool.com>
2026-09-24 19:03:32 +0300

    Built budgeting calculation functions

--- index.js
+++ index.js
@@ -5,3 +5,40 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount){
+    const taxValue = amount*10/100;
+    return taxValue;
+}
+
+console.log(calculateTax(100));
+
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+console.log(convertToUpperCase("hello"));
+
+function findMaximum(num1, num2){
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+console.log(findMaximum(5, 20));
+
+function isPalindrome(word){
+    if(word === word.split('').reverse().join('')){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    const discountedPrice = originalPrice - (originalPrice * discountPercentage / 100);
+    return discountedPrice;
+}
+console.log(calculateDiscountedPrice(100, 20));
